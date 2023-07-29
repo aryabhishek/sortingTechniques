7c732888025b3ad61d39966c6c1efa4a1b8b4e6b
Aryabhishek Verma <aryabhishekverma@gmail.com>
2023-07-29 16:37:57 +0530

    done

---
+++ BinarySearch/BsOn1dArrays/findFirstAndLastOccurance.py
@@ -0,0 +1,16 @@
+from lowerBound import lower_bound
+from upperBound import upper_bound
+
+def find_first_and_last_occurance(arr, k):
+    lb = lower_bound(arr, k)
+    n = len(arr)
+
+    if lb == n or arr[lb] != k: return [-1, -1]
+
+    return lb, upper_bound(arr, k) -1
+
+if __name__ == "__main__":
+    nums = [1,1,1,2,3,3,3]
+    target = 3
+
+    print(find_first_and_last_occurance(nums, target))
