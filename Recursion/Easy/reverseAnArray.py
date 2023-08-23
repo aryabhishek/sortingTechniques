504d94809a524cc5c69d2818950cfaa80f6162f9
Aryabhishek Verma <aryabhishekverma@gmail.com>
2023-08-23 23:09:03 +0530

    done

--- Recursion/Easy/reverseAnArray.py
+++ Recursion/Easy/reverseAnArray.py
@@ -17,7 +17,25 @@ def reverse(arr: list) -> None:
     insert(arr, last)
 
 
+# two variables
+def swap_reverse(arr, l, r):
+    if l >= r:
+        return
+    
+    arr[l], arr[r] = arr[r], arr[l]
+    swap_reverse(arr, l+1, r-1)
+
+
+# one variable
+def reverse_arr(arr, i):
+    if i >= len(arr)//2:
+        return
+    
+    arr[i], arr[len(arr)-i-1] = arr[len(arr)-i-1], arr[i]
+    reverse_arr(arr, i+1)
+
+
 if __name__ == "__main__":
     ar = [1, 2, 3, 4, 5]
-    reverse(ar)
+    reverse_arr(ar, 0)
     print(ar)
