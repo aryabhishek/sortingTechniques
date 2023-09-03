2fdab6a9f2ea106f1d791d527348aa3e8ab98c40
Aryabhishek Verma <aryabhishekverma@gmail.com>
2023-09-03 20:37:33 +0530

    Co-authored-by: mkvmb.6@gmail.com <mkvmb.6@gmail.com>

--- Recursion/PracticeProblems/fiboInReverse.py
+++ Recursion/PracticeProblems/fiboInReverse.py
@@ -1,8 +1,19 @@
+import os
+
 def fibo(n: int) -> int:
     if n <= 1:
         return n
+    fib = fibo(n-1) + fibo(n-2)
+    print(fib)
+    return fib
 
-    return fibo(n-1) + fibo(n-2)
+
+def fib2(n: int, x: int, y: int) -> None:
+    if n <= 1:
+        print(x)
+        return
+    fib2(n - 1, y, x + y)
+    print(x)
 
 
 def print_fibo_in_reverse(n: int) -> None:
@@ -15,4 +26,7 @@ def print_fibo_in_reverse(n: int) -> None:
 
 if __name__ == "__main__":
     n = 10
-    print_fibo_in_reverse(n)
+    # print_fibo_in_reverse(n)
+    os.system("cls")
+    fib2(n, 0, 1)
+    my_fib(n, 0, 1)
