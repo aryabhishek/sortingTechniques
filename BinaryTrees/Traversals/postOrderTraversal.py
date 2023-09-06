761e994b8ea459b90191cd4f0ea359d3cca0675b
Aryabhishek Verma <aryabhishekverma@gmail.com>
2023-09-07 01:47:06 +0530

    done

--- BinaryTrees/Traversals/postOrderTraversal.py
+++ BinaryTrees/Traversals/postOrderTraversal.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 def recursive(root):
     if not root:
         return
@@ -7,5 +9,18 @@ def recursive(root):
     print(root.val)
 
 
-def iterative(root):
-    pass
+def iterative(root): # two stacks
+    stk1 = deque()
+    stk2 = deque()
+    stk1.append(root)
+    while stk1:
+        node = stk1[-1]
+        stk1.pop()
+        stk2.append(node)
+        if node.left: stk1.append(node.left)
+        if node.right: stk1.append(node.right)
+
+    while stk2:
+        data = stk2[-1].val
+        stk2.pop()
+        print(data)
