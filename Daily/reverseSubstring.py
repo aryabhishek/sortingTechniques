35f481ebe2f9d9b2bf6aac12279e11f5769475a4
Aryabhishek Verma <aryabhishekverma@gmail.com>
2024-07-12 01:46:19 +0530

    done

---
+++ Daily/reverseSubstring.py
@@ -0,0 +1,26 @@
+"""
+You are given a string s that consists of lower case English letters and brackets.
+
+Reverse the strings in each pair of matching parentheses, starting from the innermost one.
+
+Your result should not contain any brackets.
+"""
+
+from collections import deque
+
+
+class Solution:
+    def reverseParentheses(self, s: str) -> str:
+        stack = deque()
+
+        for char in s:
+            if char == ")":
+                temp = []
+                while stack and stack[-1] != "(":
+                    temp.append(stack.pop())
+                stack.pop()
+                stack.extend(temp)
+            else:
+                stack.append(char)
+
+        return "".join(stack)
