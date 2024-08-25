0dedf171b902bd75321763527c81014498cf459c
Aryabhishek Verma <48897634+aryabhishek@users.noreply.github.com>
2024-08-25 18:51:57 +0530

    Update sortZerosOnesTwos.py

--- ArrayProblems/Medium/sortZerosOnesTwos.py
+++ ArrayProblems/Medium/sortZerosOnesTwos.py
@@ -1,3 +1,13 @@
+"""
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library's sort function.
+
+https://leetcode.com/problems/sort-colors/
+"""
+
 def my_solution(nums): # better than sorting, TC: O(2n), SC: O(1)
     zero = 0
     one = 0
