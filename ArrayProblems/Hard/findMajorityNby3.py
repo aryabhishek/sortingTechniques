49d67a74d65a1e0616b89190e9bd962e0947f585
Aryabhishek Verma <48897634+aryabhishek@users.noreply.github.com>
2024-08-25 10:02:58 +0530

    Update findMajorityNby3.py

--- ArrayProblems/Hard/findMajorityNby3.py
+++ ArrayProblems/Hard/findMajorityNby3.py
@@ -1,3 +1,9 @@
+"""
+Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.
+
+https://leetcode.com/problems/majority-element-ii/
+"""
+
 def solution(arr):
     n = len(arr)
 
