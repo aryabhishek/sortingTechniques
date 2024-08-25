c896f1bdba4262aa2c333f18f8306bb080e0d22e
Aryabhishek Verma <48897634+aryabhishek@users.noreply.github.com>
2024-08-25 18:53:05 +0530

    Update subarraysWithSumK.py

--- ArrayProblems/Medium/subarraysWithSumK.py
+++ ArrayProblems/Medium/subarraysWithSumK.py
@@ -1,3 +1,11 @@
+"""
+Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+https://leetcode.com/problems/subarray-sum-equals-k/
+"""
+
 def brute_force(nums, k): # TC: O(n^3), SC: O(1)
 
     def gen_subarrays(arr):
